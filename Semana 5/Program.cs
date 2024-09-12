cf1243cd0e8fd4d70a0037078bb9e65418ce5f5d
ManuelSTP1201 <juanmasantacruz12@gmail.com>
2024-09-12 08:16:28 -0500

    otro punto de salvado

--- Semana 5/Program.cs
+++ Semana 5/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿Console.WriteLine("Hello, World!");
+string re = "s";
+double suma = 0;
+double cant = 0;
+while (re == "s")
+{
+    Console.WriteLine("Desea participar? (s/n)");
+    re = Console.ReadLine().Trim().ToLower();
+    if (re == "s")
+    {
+        Console.WriteLine("Ingrese edad");
+        int edad = int.Parse(Console.ReadLine());
+        suma = suma + edad;
+        cant = cant + 1;
+    }
+}
+double prom = suma / cant;
+Console.WriteLine($"El promedio de {cant} edades es {prom}"); 
+Console.ReadKey();
--- Semana 5/obj/Debug/net8.0/Semana 5.AssemblyInfo.cs
+++ Semana 5/obj/Debug/net8.0/Semana 5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semana 5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8fa71be3deb3e2867fd56536f946e7eb4fce7e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semana 5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semana 5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
